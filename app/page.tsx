d72ab548bf4a8a2d14d17abb8d2615a9d5f8ad2e
Prajjwal <prajjwal.141820@gmail.com>
2026-02-07 14:35:54 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center h-screen">
       <Button >
         Hey There!
       </Button>
